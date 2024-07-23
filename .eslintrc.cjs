93e1367998dc7d018bf68e678f0c49ac109b42ca
viktoriabakun <viktoriabakun00@gmail.com>
2024-07-23 17:17:56 +0200

    feat: add storybook to the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,6 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:import/errors',
-    "eslint-config-prettier"
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:import/errors', 'eslint-config-prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'import', 'prettier'],
